3d75bf1a097cf859ca698daed7916dbfcf9217ec
Brotherhood94 <alessandro.berti94@hotmail.it>
2019-05-29 00:30:39 +0200

    new

--- Mid3_CNN/cnn.py
+++ Mid3_CNN/cnn.py
@@ -4,23 +4,23 @@ import os, sys, fnmatch
 import numpy as np
 
 def unpickle(path, match):
-    res_data = np.ndarray([], dtype=np.uint8)
+    res_data = np.empty((0,3072))
     res_labels = np.ndarray([])
     res_filenames = np.ndarray([])
     for file in os.listdir(path):
         if fnmatch.fnmatch(file, match):
             with open(path+file, 'rb') as fo:
                 dict = pickle.load(fo, encoding='bytes')
-                res_data = np.append(res_data, dict[b'data'])
+                res_data = np.append(res_data, dict[b'data'], axis=0)
                 res_labels = np.append(res_labels, dict[b'labels'])
                 res_filenames = np.append(res_filenames, dict[b'filenames'])
-                #print(dict[b'data'])
-                print(res_data.size)
-                #print(res_data)
+                print("########################")
+                print(dict[b'data'].shape)
+                print(res_data.shape)
     return {'data' : res_data, 'labels' : res_labels, 'filenames' : res_filenames} 
 
 def numpytoimg(dataset, index):
-    plt.imshow(dataset['data'].reshape((32, 32, 3), order='F').swapaxes(0,1))
+    plt.imshow(dataset['data'][index].reshape((32, 32, 3), order='F').swapaxes(0,1))
     print(dataset['filenames'][index].decode('utf8'))
     print(dataset['labels'][index])
     plt.show()
